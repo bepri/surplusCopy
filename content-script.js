60d823b56babc0d91f40423ff4ea1070d0c8e064
37h4nn4ug13 <nnaugle1@com1980.eecs.utk.edu>
2022-04-12 09:16:40 -0400

    Full string has right info

--- content-script.js
+++ content-script.js
@@ -1,15 +1,13 @@
 
 function getOutput() {
     let name = document.getElementById("TitleBox--_Asset_Display_html--asset-basics----QmFzaWNz---0").getElementsByClassName("asset-name");
-    
-    console.log(name[0].innerText);
     let description = document.getElementById("TitleBox--_Asset_Display_html--asset-basics----QmFzaWNz---0").getElementsByClassName("asset-description");
-    console.log(description[0].innerText)
-
     let serial = document.getElementById("CF-19-ShowRow").getElementsByClassName("value");
-    console.log(serial[0].innerText);
     let tag = document.getElementById("CF-23-ShowRow").getElementsByClassName("value");
-    console.log(tag[0].innerText)
+    
+    let fullStr = name[0].innerText.substring(6) + "\n" + description[0].innerText.substring(13) + "\n" + "Serial Number: " + serial[0].innerText + "\n" + "UT Tag: " + tag[0].innerText;
+    
+    console.log(fullStr);
 }
 
 let surplusBtn = document.getElementById(
