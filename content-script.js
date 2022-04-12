f93bb77fb718bd218b62e892591255c904377e8e
Ethan <n.ethan.naugle@gmail.com>
2022-04-11 23:34:49 -0400

    Pulled out values

--- content-script.js
+++ content-script.js
@@ -1,9 +1,15 @@
 
 function getOutput() {
-    let name = document.getElementById("TitleBox--_Asset_Display_html--asset-basics----QmFzaWNz---0").innerHTML;
-    console.log(name);
-    let serial = document.getElementById("TitleBox--_Asset_Display_html--asset-info-cfs_asset-info-cfs-Asset_Info----QXNzZXQgSW5mbw__---0").innerHTML;
-    console.log(serial);
+    let name = document.getElementById("TitleBox--_Asset_Display_html--asset-basics----QmFzaWNz---0").getElementsByClassName("asset-name");
+    
+    console.log(name[0].innerText);
+    let description = document.getElementById("TitleBox--_Asset_Display_html--asset-basics----QmFzaWNz---0").getElementsByClassName("asset-description");
+    console.log(description[0].innerText)
+
+    let serial = document.getElementById("CF-19-ShowRow").getElementsByClassName("value");
+    console.log(serial[0].innerText);
+    let tag = document.getElementById("CF-23-ShowRow").getElementsByClassName("value");
+    console.log(tag[0].innerText)
 }
 
 let surplusBtn = document.getElementById(
