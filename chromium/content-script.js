3f952a950c2be7f1e28b138f180f6e7d9864ca52
Imani Pelton <imanipelton@hotmail.com>
2023-12-21 11:22:36 -0500

    Moves old Chromium content

---
+++ chromium/content-script.js
@@ -0,0 +1,18 @@
+
+function getOutput() {
+    let name = document.getElementById("TitleBox--_Asset_Display_html--asset-basics----QmFzaWNz---0").getElementsByClassName("asset-name");
+    let description = document.getElementById("TitleBox--_Asset_Display_html--asset-basics----QmFzaWNz---0").getElementsByClassName("asset-description");
+    let serial = document.getElementById("CF-19-ShowRow").getElementsByClassName("value");
+    let tag = document.getElementById("CF-23-ShowRow").getElementsByClassName("value");
+    
+    let fullStr = name[0].innerText.substring(6) + "\n" + description[0].innerText.substring(13) + "\n" + "S/N: " + serial[0].innerText + "\n" + "Tag: " + tag[0].innerText;
+
+    console.log(fullStr);
+    let copied = navigator.clipboard.writeText(fullStr);
+}
+
+let surplusBtn = document.getElementById(
+    "TitleBox--_Asset_Display_html--asset-info-cfs_asset-info-cfs-Asset_Info----QXNzZXQgSW5mbw__---0"
+    );
+surplusBtn.innerHTML += "<button id='surplusBtn'>Copy surplus information</button>";
+document.getElementById("surplusBtn").addEventListener("click", getOutput);
